1a6813617e0ffe864aab84fc3ca8b02379409697
wuliguaiguaia <zhouyuanli@zuoyebang.com>
2020-01-31 21:14:13 +0800

    feat: 添加chai单元测试

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import "./icon/iconfont.scss";
 
 import Button from './components/button/button';
 import ButtonGroup from "./components/button/button-group"
+
 Vue.component('z-button', Button);
 Vue.component('z-button-group', ButtonGroup);
 
@@ -15,3 +16,86 @@ new Vue({
         }
     }
 })
+
+
+// 单元测试
+import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies);
+const expect = chai.expect;
+
+{
+    const constructor = Vue.extend(Button);
+    const vm = new constructor({
+        propsData: {
+            type: 'primary'
+        }
+    });
+    vm.$mount();
+    let el = vm.$el;
+    expect(el.classList.contains('z-button--primary')).to.be.true;
+    el.remove();
+    vm.$destroy();
+}
+{
+    const constructor = Vue.extend(Button);
+    const vm = new constructor({
+        propsData: {
+            nativeType: 'submit'
+        }
+    });
+    vm.$mount();
+    let el = vm.$el;
+    expect(el.getAttribute('type')).to.equal('submit');
+    el.remove();
+    vm.$destroy(); 
+}
+
+{
+    // 测试click - 1
+    const constructor = Vue.extend(Button);
+    const vm = new constructor();
+    vm.$on('click', _=>{
+        expect(1).to.equal(1)
+    })
+    vm.$mount();
+    vm.$el.click();
+    vm.$el.remove();
+    vm.$destroy();
+}
+
+{
+    // 测试click - 2 fail
+    let result;
+    const vm = new Vue({
+        template: `<z-button @click=handleClick></z-button>`,
+        methods:{
+            handleClick (evt) {
+                console.log(evt);
+                result = evt;
+            }
+        }
+    }).$mount();
+    vm.$el.click();
+    setTimeout(_=>{
+        console.log(result);
+        expect(result).to.exist;
+        vm.$el.remove();
+        vm.$destroy();
+    }, 20)
+}
+
+{
+    // 测试click - 1
+    const constructor = Vue.extend(Button);
+    const vm = new constructor();
+    vm.$mount();
+    let el = vm.$el;
+    let spy = chai.spy(function(){});
+    vm.$on('click', spy);
+    el.click();
+    // 确保被执行
+    expect(spy).to.have.been.called()
+    el.remove();
+    vm.$destroy();
+}
