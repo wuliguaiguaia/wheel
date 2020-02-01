fd6454f2f776a69d81f78bf7c857b2ffe581ebeb
wuliguaiguaia <zhouyuanli@zuoyebang.com>
2020-02-01 18:51:12 +0800

    fix: 单独打包index.js解决sass-loader报错

---
+++ dist/index.js
@@ -0,0 +1,220 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+	// Save the require from previous bundle to this closure if any
+	var previousRequire = typeof parcelRequire === "function" && parcelRequire;
+	var nodeRequire = typeof require === "function" && require;
+
+	function newRequire(name, jumped) {
+		if (!cache[name]) {
+			if (!modules[name]) {
+				// if we cannot find the module within our internal map or
+				// cache jump to the current global require ie. the last bundle
+				// that was added to the page.
+				var currentRequire = typeof parcelRequire === "function" && parcelRequire;
+				if (!jumped && currentRequire) {
+					return currentRequire(name, true);
+				}
+
+				// If there are other bundles on this page the require from the
+				// previous one is saved to 'previousRequire'. Repeat this as
+				// many times as there are bundles until the module is found or
+				// we exhaust the require chain.
+				if (previousRequire) {
+					return previousRequire(name, true);
+				}
+
+				// Try the node require function if it exists.
+				if (nodeRequire && typeof name === "string") {
+					return nodeRequire(name);
+				}
+
+				var err = new Error("Cannot find module '" + name + "'");
+				err.code = "MODULE_NOT_FOUND";
+				throw err;
+			}
+
+			localRequire.resolve = resolve;
+			localRequire.cache = {};
+
+			var module = cache[name] = new newRequire.Module(name);
+
+			modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+		}
+
+		return cache[name].exports;
+
+		function localRequire(x){
+			return newRequire(localRequire.resolve(x));
+		}
+
+		function resolve(x){
+			return modules[name][1][x] || x;
+		}
+	}
+
+	function Module(moduleName) {
+		this.id = moduleName;
+		this.bundle = newRequire;
+		this.exports = {};
+	}
+
+	newRequire.isParcelRequire = true;
+	newRequire.Module = Module;
+	newRequire.modules = modules;
+	newRequire.cache = cache;
+	newRequire.parent = previousRequire;
+	newRequire.register = function (id, exports) {
+		modules[id] = [function (require, module) {
+			module.exports = exports;
+		}, {}];
+	};
+
+	var error;
+	for (var i = 0; i < entry.length; i++) {
+		try {
+			newRequire(entry[i]);
+		} catch (e) {
+			// Save first error but execute all entries
+			if (!error) {
+				error = e;
+			}
+		}
+	}
+
+	if (entry.length) {
+		// Expose entry point to Node, AMD or browser globals
+		// Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+		var mainExports = newRequire(entry[entry.length - 1]);
+
+		// CommonJS
+		if (typeof exports === "object" && typeof module !== "undefined") {
+			module.exports = mainExports;
+
+			// RequireJS
+		} else if (typeof define === "function" && define.amd) {
+			define(function () {
+				return mainExports;
+			});
+
+			// <script>
+		} else if (globalName) {
+			this[globalName] = mainExports;
+		}
+	}
+
+	// Override the current require with this new one
+	parcelRequire = newRequire;
+
+	if (error) {
+		// throw error from earlier, _after updating parcelRequire_
+		throw error;
+	}
+
+	return newRequire;
+})({"FYHv":[function(require,module,exports) {
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.default = void 0;
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	var _default = {
+		name: "z-button",
+		props: {
+			type: {
+				type: String,
+				default: "default",
+				validator: function validator(val) {
+					return ["default", "primary", "success", "warning", "danger", "info", "text"].includes(val);
+				}
+			},
+			nativeType: {
+				type: String,
+				default: "button",
+				validator: function validator(val) {
+					return ["button", "submit", "reset"].includes(val);
+				}
+			},
+			size: {
+				type: String,
+				validator: function validator(val) {
+					return ["medium", "small", "mini"].includes(val);
+				}
+			},
+			loading: Boolean,
+			icon: String,
+			plain: Boolean,
+			round: Boolean,
+			circle: Boolean,
+			disabled: Boolean
+		},
+		computed: {
+			buttonClass: function buttonClass() {
+				return ["z-button", this.type ? "z-button--" + this.type : "", this.size ? "z-button--" + this.size : "", {
+					"is-plain": this.plain,
+					"is-round": this.round,
+					"is-circle": this.circle,
+					"is-disabled": this.disabled
+				}];
+			}
+		}
+	};
+	exports.default = _default;
+	var $6a6a5e = exports.default || module.exports;
+      
+	if (typeof $6a6a5e === "function") {
+		$6a6a5e = $6a6a5e.options;
+	}
+    
+	/* template */
+	Object.assign($6a6a5e, (function () {
+		var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c("button",{ref:"button",class:_vm.buttonClass,attrs:{"disabled":_vm.disabled,"type":_vm.nativeType,"autofocus":"autofocus"},on:{"click":function($event){return _vm.$emit("click");}}},[(_vm.icon && !_vm.loading)?_c("i",{class:_vm.icon}):_vm._e(),_vm._v(" "),(_vm.loading)?_c("i",{staticClass:"icon-arrows_anticlockwise_dashed icon-loading"}):_vm._e(),_vm._v(" "),_vm._t("default")],2);};
+		var staticRenderFns = [];
+
+		return {
+			render: render,
+			staticRenderFns: staticRenderFns,
+			_compiled: true,
+			_scopeId: "data-v-6a6a5e",
+			functional: undefined
+		};
+	})());
+      
+},{}],"Focm":[function(require,module,exports) {
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.default = void 0;
+
+	var _button = _interopRequireDefault(require("./src/components/button/button"));
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var _default = {
+		Button: _button.default
+	};
+	exports.default = _default;
+},{"./src/components/button/button":"FYHv"}]},{},["Focm"], null);
+//# sourceMappingURL=/index.js.map
